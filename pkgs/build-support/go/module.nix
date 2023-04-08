8ff77975c9c63b46837954411ab7d67adcbc78e3
Yueh-Shun Li <44064051+ShamrockLee@users.noreply.github.com>
2023-04-08 18:13:57 +0800

    buildGoModule: Fix overriding with overlay-style stdenv
    Refactor `buildGoModule` to adopt the overlay-style `stdenv.mkDerivation`
    and to fix overriding.
    
    `vendorHash` can now be overridden directly using `overrideAttrs`.
    
    The `go-modules`, `vendorHash` and `vendorSha256` attributes are
    moved out of `passthru` as they now already exposed as
    attributes of `stdenv.mkDerivation`.
    
    The builder now only adds `go` to `passthru`.

--- pkgs/build-support/go/module.nix
+++ pkgs/build-support/go/module.nix
@@ -5,100 +5,173 @@
 , go
 }:
 
-{ name ? "${args'.pname}-${args'.version}"
-, src
-, buildInputs ? [ ]
-, nativeBuildInputs ? [ ]
-, passthru ? { }
-, patches ? [ ]
-
-  # Go linker flags, passed to go via -ldflags
-, ldflags ? [ ]
-
-  # Go tags, passed to go via -tag
-, tags ? [ ]
-
-  # A function to override the go-modules derivation
-, overrideModAttrs ? (_oldAttrs: { })
-
-  # path to go.mod and go.sum directory
-, modRoot ? "./"
-
-  # vendorHash is the SRI hash of the vendored dependencies
-  #
-  # if vendorHash is null, then we won't fetch any dependencies and
-  # rely on the vendor folder within the source.
-, vendorHash ? "_unset"
-  # same as vendorHash, but outputHashAlgo is hardcoded to sha256
-  # so regular base32 sha256 hashes work
-, vendorSha256 ? "_unset"
-  # Whether to delete the vendor folder supplied with the source.
-, deleteVendor ? false
-  # Whether to fetch (go mod download) and proxy the vendor directory.
-  # This is useful if your code depends on c code and go mod tidy does not
-  # include the needed sources to build or if any dependency has case-insensitive
-  # conflicts which will produce platform dependant `vendorHash` checksums.
-, proxyVendor ? false
-
-  # We want parallel builds by default
-, enableParallelBuilding ? true
-
-  # Do not enable this without good reason
-  # IE: programs coupled with the compiler
-, allowGoReference ? false
-
-, CGO_ENABLED ? go.CGO_ENABLED
-
-, meta ? { }
-
-  # Not needed with buildGoModule
-, goPackagePath ? ""
-
-  # needed for buildFlags{,Array} warning
-, buildFlags ? ""
-, buildFlagsArray ? ""
-
-, ...
-}@args':
-
-with builtins;
-
-assert goPackagePath != "" -> throw "`goPackagePath` is not needed with `buildGoModule`";
-assert (vendorSha256 == "_unset" && vendorHash == "_unset") -> throw "either `vendorHash` or `vendorSha256` is required";
-assert (vendorSha256 != "_unset" && vendorHash != "_unset") -> throw "both `vendorHash` and `vendorSha256` set. only one can be set.";
+# Documentation about attributes can be found in comment blocks
+# begin with `ATTRIBUTES:`
 
 let
-  hasAnyVendorHash = (vendorSha256 != null && vendorSha256 != "_unset") || (vendorHash != null && vendorHash != "_unset");
-  vendorHashType =
-    if hasAnyVendorHash then
-      if vendorSha256 != null && vendorSha256 != "_unset" then
-        "sha256"
-      else
-        "sri"
-    else
-      null;
+  # This helper function moves "special attributes" into `passthru.__onetimeAttrs`
+  # to prevent string coertion.
+  # One use case here is `overrideModAttrs`
+  collectOnetimeAttrs = names: attrs:
+    (removeAttrs attrs names) // {
+      passthru = attrs.passthru or { } // {
+        __onetimeAttrs = builtins.foldl'
+          (attrsCollected: name:
+            attrsCollected // lib.optionalAttrs (builtins.elem name names) {
+              "${name}" = attrs.${name};
+            })
+          { }
+          (builtins.attrNames attrs);
+      };
+    };
+in
+rattrs:
+# Upgrade and overriding:
+#
+# Prefetch the vendorHash:
+# ```sh
+# nix-prefetch -E "{ sha256 }: ((import ./. { }).my-package.overrideAttrs (_: { vendorHash = sha256; })).go-modules"
+# ```
+#
+# Override the vendorHash:
+# ```nix
+# my-package.overrideAttrs (_: {
+#   src = fetchFromGitHub { ... };
+#   vendorHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA88224466BA=";
+# })
+# ```
+#
+# Note that overrideModAttrs and mod*Phase are merely syntax sugar for packaging,
+# and is not available when overriding. Override go-modules directly instead:
+# ```nix
+# my-package.overrideAttrs (finalAttrs: previousAttrs: {
+#   go-modules = previousAttrs.go-modules.overrideAttrs (finalModAttrs: prevModAttrs: {
+#     configurePhase = ...;
+#     preBuild = ...;
+#     buildPhase = ...;
+#     installPhase = ...;
+#   });
+# })
+# ```
 
-  args = removeAttrs args' [ "overrideModAttrs" "vendorSha256" "vendorHash" ];
+((stdenv.mkDerivation (as:
+  collectOnetimeAttrs
+    [
+      # ATTRIBUTES: These attributes won't be passed down to later overlays
 
-  go-modules =
-    if (!hasAnyVendorHash) then "" else
-    (stdenv.mkDerivation {
+      # Function with style `(previousAttrs: { ... })`.
+      # It is syntax sugar to override go-modules,
+      # equivalent to `go-modules.overrideAttrs overrideModAttrs`
+      "overrideModAttrs"
 
-      name = "${name}-go-modules";
+      # Syntax sugar to manually specify the corresponding phases
+      # of the `go-modules` sub-derivation.
+      "modConfigurePhase"
+      "modBuildPhase"
+      "modeInstallPhase"
+    ]
+    (if lib.isFunction rattrs then rattrs as else rattrs)
+)).overrideAttrs (finalAttrs: previousAttrs:
+  let
+    hasAnyVendorHash = finalAttrs.vendorHash != null && finalAttrs.vendorHash != "_unset" || finalAttrs.vendorSha256 != null && finalAttrs.vendorSha256 != "_unset";
+    inherit (previousAttrs.passthru) __onetimeAttrs;
+  in
+  {
+    # ATTRIBUTES:
+    # These attributes will be given default values
+    # when not set previously
 
-      nativeBuildInputs = (args.nativeBuildInputs or [ ]) ++ [ go git cacert ];
+    # Go linker flags, passed to go via -ldflags
+    ldflags = previousAttrs.ldflags or [ ];
 
-      inherit (args) src;
+    # Go tags, passed to go via -tag
+    tags = previousAttrs.tags or [ ];
+
+    # path to go.mod and go.sum directory
+    modRoot = previousAttrs.modRoot or "./";
+
+    # vendorHash is the SRI hash of the vendored dependencies
+    #
+    # if vendorHash is null, then we won't fetch any dependencies and
+    # rely on the vendor folder within the source.
+    vendorHash = previousAttrs.vendorHash or "_unset";
+
+    # same as vendorHash, but outputHashAlgo is hardcoded to sha256
+    # so regular base32 sha256 hashes work
+    vendorSha256 = previousAttrs.vendorSha256 or "_unset";
+
+    # Whether to delete the vendor folder supplied with the source.
+    deleteVendor = previousAttrs.deleteVendor or false;
+    # Whether to fetch (go mod download) and proxy the vendor directory.
+    # This is useful if your code depends on c code and go mod tidy does not
+    # include the needed sources to build or if any dependency has case-insensitive
+    # conflicts which will produce platform dependant `vendorHash` checksums.
+    proxyVendor = previousAttrs.proxyVendor or false;
+
+    # We want parallel builds by default
+    enableParallelBuilding = previousAttrs.enableParallelBuilding or true;
+
+    # Do not enable this without good reason
+    # IE: programs coupled with the compiler
+    allowGoReference = previousAttrs.allowGoReference or false;
+
+    CGO_ENABLED = previousAttrs.CGO_ENABLED or go.CGO_ENABLED;
+
+    # Default to run the tests
+    doCheck = previousAttrs.doCheck or true;
+
+    # These phases will be shared with the 'go-modules` sub-derivation
+    # so we give them default values if not specified.
+    preUnpack = previousAttrs.preUnpack or null;
+    unpackPhase = previousAttrs.unapckPhase or null;
+    postUnpack = previousAttrs.postUnpack or null;
+    sourceRoot = previousAttrs.sourceRoot or null;
+    prePatch = previousAttrs.prePatch or null;
+    patches = previousAttrs.patches or [ ];
+    patchFlags = previousAttrs.patchFlags or [ ];
+    patchPhase = previousAttrs.patchPhase or null;
+    postPatch = previousAttrs.postPatch or null;
+    preBuild = previousAttrs.preBuild or null;
+    postBuild = previousAttrs.postBuild or null;
+
+    # `configurePhase`, `buildPhase` and `checkPhase` attributes can be set,
+    # but their default values are the implementation of this builder,
+    # so leave them unset unless you need to overwrite them.
+    #
+    # If you would like to use `stdenv.mkDerivation`-provided phases,
+    # set the corresponding phases to `null`.
+
+
+    # ATTRIBUTES:
+    # These attributes must not be set.
+    # Here we feed them dummy values for later assertions.
+
+    goPackagePath = previousAttrs.goPackagePath or "";
+    buildFlags = previousAttrs.buildFlags or "";
+    buildFlagsArray = previousAttrs.buildFlagsArray or "";
+
+
+    # ATTRIBUTES:
+    # These attributes will be appended:
+    # `buildInputs` `passthru` `meta`
+
+    # Other attributes set below will be directly overriten by this builder.
+    # Use overrideAttrs if you really want to modify them.
+
+    # End of the ATTRIBUTES documentation blocks
+
+
+    # Fixed output derivation containing Go modules
+    go-modules = if (!hasAnyVendorHash) then "" else
+    (stdenv.mkDerivation (finalModAttrs: {
+
+      nativeBuildInputs = finalAttrs.nativeBuildInputs or [ ] ++ [ cacert git go ];
+
+      inherit (finalAttrs) src;
+
+      inherit (finalAttrs) prePatch patches patchFlags postPatch preBuild postBuild sourceRoot;
+
       inherit (go) GOOS GOARCH;
-
-      prePatch = args.prePatch or "";
-      patches = args.patches or [ ];
-      patchFlags = args.patchFlags or [ ];
-      postPatch = args.postPatch or "";
-      preBuild = args.preBuild or "";
-      postBuild = args.postBuild or "";
-      sourceRoot = args.sourceRoot or "";
-
       GO111MODULE = "on";
 
       impureEnvVars = lib.fetchers.proxyImpureEnvVars ++ [
@@ -107,17 +180,17 @@ let
         "GOPROXY"
       ];
 
-      configurePhase = args.modConfigurePhase or ''
+      configurePhase = __onetimeAttrs.modConfigurePhase or ''
         runHook preConfigure
         export GOCACHE=$TMPDIR/go-cache
         export GOPATH="$TMPDIR/go"
-        cd "${modRoot}"
+        cd "${finalAttrs.modRoot}"
         runHook postConfigure
       '';
 
-      buildPhase = args.modBuildPhase or (''
+      buildPhase = __onetimeAttrs.modBuildPhase or (''
         runHook preBuild
-      '' + lib.optionalString deleteVendor ''
+      '' + lib.optionalString finalAttrs.deleteVendor ''
         if [ ! -d vendor ]; then
           echo "vendor folder does not exist, 'deleteVendor' is not needed"
           exit 10
@@ -130,7 +203,7 @@ let
           exit 10
         fi
 
-        ${if proxyVendor then ''
+        ${if finalAttrs.proxyVendor then ''
           mkdir -p "''${GOPATH}/pkg/mod/cache/download"
           go mod download
         '' else ''
@@ -145,10 +218,10 @@ let
         runHook postBuild
       '');
 
-      installPhase = args.modInstallPhase or ''
+      installPhase = __onetimeAttrs.modInstallPhase or ''
         runHook preInstall
 
-        ${if proxyVendor then ''
+        ${if finalAttrs.proxyVendor then ''
           rm -rf "''${GOPATH}/pkg/mod/cache/download/sumdb"
           cp -r --reflink=auto "''${GOPATH}/pkg/mod/cache/download" $out
         '' else ''
@@ -166,20 +239,24 @@ let
       dontFixup = true;
 
       outputHashMode = "recursive";
-      outputHashAlgo = if vendorHashType == "sha256" then "sha256" else null;
-      outputHash = if vendorHashType == "sha256" then vendorSha256 else vendorHash;
-    }).overrideAttrs overrideModAttrs;
+      outputHashAlgo = if finalAttrs.vendorSha256 != "_unset" then "sha256" else null;
+      outputHash = if finalAttrs.vendorHash != "_unset" then finalAttrs.vendorHash else finalAttrs.vendorSha256;
+    }
+    // (if (previousAttrs ? pname && previousAttrs.pname != "" && previousAttrs ? version) then {
+      pname = "go-modules-${finalAttrs.pname}";
+      inherit (finalAttrs) version;
+    } else {
+      name = "go-modules-${finalAttrs.name}";
+    }))).overrideAttrs __onetimeAttrs.overrideModAttrs or (_: { });
 
-  package = stdenv.mkDerivation (args // {
-    nativeBuildInputs = [ go ] ++ nativeBuildInputs;
+    nativeBuildInputs = [ go ] ++ previousAttrs.nativeBuildInputs or [ ];
 
     inherit (go) GOOS GOARCH;
 
     GO111MODULE = "on";
-    GOFLAGS = lib.optionals (!proxyVendor) [ "-mod=vendor" ] ++ lib.optionals (!allowGoReference) [ "-trimpath" ];
-    inherit CGO_ENABLED enableParallelBuilding;
+    GOFLAGS = lib.optionals (!finalAttrs.proxyVendor) [ "-mod=vendor" ] ++ lib.optionals (!finalAttrs.allowGoReference) [ "-trimpath" ];
 
-    configurePhase = args.configurePhase or (''
+    configurePhase = previousAttrs.configurePhase or (''
       runHook preConfigure
 
       export GOCACHE=$TMPDIR/go-cache
@@ -188,11 +265,11 @@ let
       export GOSUMDB=off
       cd "$modRoot"
     '' + lib.optionalString hasAnyVendorHash ''
-      ${if proxyVendor then ''
-        export GOPROXY=file://${go-modules}
+      ${if finalAttrs.proxyVendor then ''
+        export GOPROXY=file://${finalAttrs.go-modules}
       '' else ''
         rm -rf vendor
-        cp -r --reflink=auto ${go-modules} vendor
+        cp -r --reflink=auto ${finalAttrs.go-modules} vendor
       ''}
     '' + ''
 
@@ -205,7 +282,7 @@ let
       runHook postConfigure
     '');
 
-    buildPhase = args.buildPhase or (''
+    buildPhase = previousAttrs.buildPhase or (''
       runHook preBuild
 
       exclude='\(/_\|examples\|Godeps\|testdata'
@@ -224,7 +301,7 @@ let
 
         declare -a flags
         flags+=($buildFlags "''${buildFlagsArray[@]}")
-        flags+=(''${tags:+-tags=${lib.concatStringsSep "," tags}})
+        flags+=(''${tags:+-tags=${lib.concatStringsSep "," finalAttrs.tags}})
         flags+=(''${ldflags:+-ldflags="$ldflags"})
         flags+=("-p" "$NIX_BUILD_CORES")
 
@@ -287,9 +364,9 @@ let
       runHook postBuild
     '');
 
-    doCheck = args.doCheck or true;
-    checkPhase = args.checkPhase or ''
+    checkPhase = previousAttrs.checkPhase or ''
       runHook preCheck
+
       # We do not set trimpath for tests, in case they reference test assets
       export GOFLAGS=''${GOFLAGS//-trimpath/}
 
@@ -300,7 +377,7 @@ let
       runHook postCheck
     '';
 
-    installPhase = args.installPhase or ''
+    installPhase = previousAttrs.installPhase or ''
       runHook preInstall
 
       mkdir -p $out
@@ -312,16 +389,27 @@ let
 
     strictDeps = true;
 
-    disallowedReferences = lib.optional (!allowGoReference) go;
+    disallowedReferences = lib.optional (!finalAttrs.allowGoReference) go;
 
-    passthru = passthru // { inherit go go-modules vendorSha256 vendorHash; };
+    passthru = (removeAttrs previousAttrs.passthru [ "__onetimeAttrs" ]) // {
+      inherit go;
+    };
 
     meta = {
       # Add default meta information
       platforms = go.meta.platforms or lib.platforms.all;
-    } // meta;
-  });
-in
-lib.warnIf (buildFlags != "" || buildFlagsArray != "")
-  "Use the `ldflags` and/or `tags` attributes instead of `buildFlags`/`buildFlagsArray`"
-  package
+    } // previousAttrs.meta or { };
+  }
+
+  # Assertions
+)).overrideAttrs (finalAttrs: previousAttrs: {
+  go-modules =
+    # Assert that either vendorHash or vendorSha256 is set.
+    assert finalAttrs.vendorHash == "_unset" && finalAttrs.vendorSha256 == "_unset" -> throw
+      "both `vendorHash` and `vendorSha256` set. only one can be set.";
+    assert finalAttrs.goPackagePath != "" -> throw
+      "`goPackagePath` is not needed with `buildGoModule`";
+    lib.warnIf (finalAttrs.buildFlags != "" || finalAttrs.buildFlagsArray != "")
+      "Use the `ldflags` and/or `tags` attributes instead of `buildFlags`/`buildFlagsArray`"
+      previousAttrs.go-modules;
+})
