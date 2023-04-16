9d03fbadd062299237e32f48f6872860786a6ceb
Yueh-Shun Li <44064051+ShamrockLee@users.noreply.github.com>
2023-04-16 21:39:13 +0000

    nix-prefetch-vscode-marketplace, nix-prefetch-openvsx: init at 0.1.0

---
+++ pkgs/by-name/ni/nix-prefetch-openvsx/package.nix
@@ -0,0 +1,71 @@
+{ stdenvNoCC
+, lib
+, makeWrapper
+, shellcheck-minimal
+, bash
+, coreutils
+, curl
+, jq
+, unzip
+, nix
+, nix-prefetch-vsix-lib
+}:
+
+stdenvNoCC.mkDerivation (finalAttrs: {
+  __structuredAttrs = true;
+
+  pname = "nix-prefetch-openvsx";
+  version = "0.1.0";
+
+  preferLocalBuild = true;
+
+  src = ./nix-prefetch-openvsx;
+
+  dontUnpack = true;
+
+  strictDeps = true;
+
+  nativeBuildInputs = [
+    makeWrapper
+  ];
+
+  buildInputs = [
+    bash
+  ];
+
+  installPhase = ''
+    runHook preInstall
+    mkdir -p "$out/bin"
+    install -m 755 -T "$src" "$out/bin/nix-prefetch-openvsx"
+    runHook postInstall
+  '';
+
+  postFixup = ''
+    wrapProgram "$out/bin/nix-prefetch-openvsx" \
+      --prefix PATH : "${lib.makeBinPath [
+        nix-prefetch-vsix-lib
+      ]}"
+  '';
+
+  doInstallCheck = true;
+
+  nativeInstallCheckInputs = [
+    shellcheck-minimal
+    nix-prefetch-vsix-lib
+  ];
+
+  installCheckPhase = ''
+    runHook preInstallCheck
+    while IFS= read -r -d "" file; do
+      shellcheck -x -P "$PATH" "$file"
+    done < <(find "$out/bin" -mindepth 1 -type f,l -print0)
+    runHook postInstallCheck
+  '';
+
+  meta = with lib; {
+    description = "Prefetch vscode extensions from Open VSX Registry";
+    license = licenses.mit;
+    platforms = platforms.all;
+    maintainers = with maintainers; [ ShamrockLee ];
+  };
+})
---
+++ pkgs/by-name/ni/nix-prefetch-vscode-marketplace/package.nix
@@ -0,0 +1,77 @@
+{ stdenvNoCC
+, lib
+, makeWrapper
+, shellcheck-minimal
+, bash
+, coreutils
+, curl
+, jq
+, unzip
+, nix
+, nix-prefetch-vsix-lib
+}:
+
+stdenvNoCC.mkDerivation (finalAttrs: {
+  __structuredAttrs = true;
+
+  pname = "nix-prefetch-vscode-marketplace";
+  version = "0.1.0";
+
+  preferLocalBuild = true;
+
+  src = ./nix-prefetch-vscode-marketplace;
+
+  dontUnpack = true;
+
+  strictDeps = true;
+
+  nativeBuildInputs = [
+    makeWrapper
+  ];
+
+  buildInputs = [
+    bash
+  ];
+
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out/bin
+    cp "$src" "$out/bin/nix-prefetch-vscode-marketplace"
+    chmod +x "$out/bin/nix-prefetch-vscode-marketplace"
+    runHook postInstall
+  '';
+
+  postFixup = ''
+    wrapProgram "$out/bin/nix-prefetch-vscode-marketplace" \
+      --prefix PATH : "${lib.makeBinPath [
+        coreutils
+        curl
+        jq
+        unzip
+        nix
+        nix-prefetch-vsix-lib
+      ]}"
+  '';
+
+  doInstallCheck = true;
+
+  nativeInstallCheckInputs = [
+    shellcheck-minimal
+    nix-prefetch-vsix-lib
+  ];
+
+  installCheckPhase = ''
+    runHook preInstallCheck
+    while IFS= read -r -d "" file; do
+      shellcheck -x -P "$PATH" "$file"
+    done < <(find "$out/bin" -mindepth 1 -type f,l -print0)
+    runHook postInstallCheck
+  '';
+
+  meta = with lib; {
+    description = "Prefetch vscode extensions from the official marketplace";
+    license = licenses.mit;
+    platforms = platforms.all;
+    maintainers = with maintainers; [ ShamrockLee ];
+  };
+})
