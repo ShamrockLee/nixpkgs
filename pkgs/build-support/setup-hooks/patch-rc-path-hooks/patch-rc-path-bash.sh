aae9055eaf92d534e710b26f5b80bf659f9c31d1
Yueh-Shun Li <44064051+ShamrockLee@users.noreply.github.com>
2023-05-03 15:38:26 +0000

    patchRcPath* hooks: simplify by removing NIXPKGS_SOURCETIME* vars
    Insert the value of $SOURCETIME_PATH directly into the target script
    instead of defining another varaible NIXPKGS_SOURCETIME_PATH.
    
    Simplify the code and eliminate the need of
    NIXPKGS_SOURCETIME_PATH_OLD to handle nested patching.
    
    Use "sed s/a/b/" instead of "sed s#a#b" when substituting paths, since
    characters other than "/" or "\0" are perfectly legal in POSIX paths.

--- pkgs/build-support/setup-hooks/patch-rc-path-hooks/patch-rc-path-bash.sh
+++ pkgs/build-support/setup-hooks/patch-rc-path-hooks/patch-rc-path-bash.sh
@@ -3,25 +3,15 @@ patchRcPathBash(){
     local SOURCETIME_PATH="$2"
     local FILE_TO_WORK_ON="$(mktemp "$(basename "$FILE_TO_PATCH").XXXXXX.tmp")"
     cat <<EOF >> "$FILE_TO_WORK_ON"
-# Lines to add to PATH the source-time utilities for Nixpkgs packaging
-if [[ -n "\${NIXPKGS_SOURCETIME_PATH-}" ]]; then
-    NIXPKGS_SOURCETIME_PATH_OLD="\$NIXPKGS_SOURCETIME_PATH;\${NIXPKGS_SOURCETIME_PATH_OLD-}"
-fi
-NIXPKGS_SOURCETIME_PATH="$SOURCETIME_PATH"
-if [[ -n "\$PATH" ]]; then
-    PATH="\$NIXPKGS_SOURCETIME_PATH:\$PATH"
-else
-    PATH="\$NIXPKGS_SOURCETIME_PATH"
-fi
-export PATH
-# End of lines to add to PATH source-time utilities for Nixpkgs packaging
+# Add to PATH the source-time utilities for Nixpkgs packaging
+PATH="$SOURCETIME_PATH\${PATH:+:}\${PATH-}"; export PATH
 EOF
     cat "$FILE_TO_PATCH" >> "$FILE_TO_WORK_ON"
     cat <<EOF >> "$FILE_TO_WORK_ON"
 # Lines to clean up inside PATH the source-time utilities for Nixpkgs packaging
 if [[ -n "\${PATH-}" ]]; then
     # Remove the inserted section
-    PATH="\${PATH/\$NIXPKGS_SOURCETIME_PATH}"
+    PATH="\${PATH/${SOURCETIME_PATH//\//\\\/}}"
     # Remove the duplicated colons
     PATH="\${PATH//::/:}"
     # Remove the prefixing colon
@@ -33,15 +23,6 @@ if [[ -n "\${PATH-}" ]]; then
         PATH="\${PATH::}"
     fi
     export PATH
-fi
-if [[ -n "\${NIXPKGS_SOURCETIME_PATH_OLD-}" ]]; then
-    IFS="" read -r -d ";" NIXPKGS_SOURCETIME_PATH <<< "\$NIXPKGS_SOURCETIME_PATH_OLD"
-    NIXPKGS_SOURCETIME_PATH_OLD="\${NIXPKGS_SOURCETIME_PATH_OLD:\${#NIXPKGS_SOURCETIME_PATH}+1}"
-else
-    unset NIXPKGS_SOURCETIME_PATH
-fi
-if [[ -z "\${NIXPKGS_SOURCETIME_PATH_OLD-}" ]]; then
-    unset NIXPKGS_SOURCETIME_PATH_OLD
 fi
 # End of lines to clean up inside PATH the source-time utilities for Nixpkgs packaging
 EOF
