2ab0ebdd085a7f54eeb5c1f21ab352ad202578a3
Yueh-Shun Li <shamrocklee@posteo.net>
2024-09-11 00:28:07 +0800

    eggUnpackHook: lint with ShellCheck

--- pkgs/development/interpreters/python/hooks/egg-unpack-hook.sh
+++ pkgs/development/interpreters/python/hooks/egg-unpack-hook.sh
@@ -1,10 +1,13 @@
 # Setup hook to use in case an egg is fetched
+# shellcheck shell=bash
+
 echo "Sourcing egg setup hook"
 
 eggUnpackPhase() {
     echo "Executing eggUnpackPhase"
     runHook preUnpack
 
+    # shellcheck disable=2154
     cp "$src" "$(stripHash "$src")"
 
     # runHook postUnpack # Calls find...?
