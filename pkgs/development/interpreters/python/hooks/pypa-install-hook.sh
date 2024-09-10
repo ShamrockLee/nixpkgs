15265b10392ba3ed5602c8ed515350e3b3774cd8
Yueh-Shun Li <shamrocklee@posteo.net>
2024-09-11 05:19:23 +0800

    pypaInstallHook: lint with ShellCheck

--- pkgs/development/interpreters/python/hooks/pypa-install-hook.sh
+++ pkgs/development/interpreters/python/hooks/pypa-install-hook.sh
@@ -1,5 +1,10 @@
 # Setup hook for PyPA installer.
+# shellcheck shell=bash
+
 echo "Sourcing pypa-install-hook"
+
+# shellcheck source=pkgs/stdenv/generic/setup.sh
+source /dev/null
 
 pypaInstallPhase() {
     echo "Executing pypaInstallPhase"
@@ -8,6 +13,7 @@ pypaInstallPhase() {
     pushd dist >/dev/null
 
     for wheel in *.whl; do
+        # shellcheck disable=2154
         @pythonInterpreter@ -m installer --prefix "$out" "$wheel"
         echo "Successfully installed $wheel"
     done
