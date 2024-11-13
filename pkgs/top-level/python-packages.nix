879cb394c6e68114eb75013b2f50464c5f204f75
Yueh-Shun Li <shamrocklee@posteo.net>
2024-11-13 20:24:58 +0800

    python3Packages.fitter, fitter: init at 1.7.1
    Co-authored-by: Fabian Affolter <mail@fabian-affolter.ch>

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4536,6 +4536,8 @@ self: super: with self; {
 
   fitbit = callPackage ../development/python-modules/fitbit { };
 
+  fitter = callPackage ../development/python-modules/fitter { };
+
   fivem-api = callPackage ../development/python-modules/fivem-api { };
 
   fixerio = callPackage ../development/python-modules/fixerio { };
