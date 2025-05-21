c39dc20210f937d84a1940b19da156b37ff4712f
Yueh-Shun Li <shamrocklee@posteo.net>
2025-05-21 21:31:44 +0800

    speechd: 0.11.5 -> 0.12.1

--- pkgs/by-name/sp/speechd/package.nix
+++ pkgs/by-name/sp/speechd/package.nix
@@ -38,11 +38,11 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "speech-dispatcher";
-  version = "0.11.5";
+  version = "0.12.1";
 
   src = fetchurl {
     url = "https://github.com/brailcom/speechd/releases/download/${version}/${pname}-${version}.tar.gz";
-    sha256 = "sha256-HOR1n/q7rxrrQzpewHOb4Gdum9+66URKezvhsq8+wSs=";
+    sha256 = "sha256-sUpSONKH0tzOTdQrvWbKZfoijn5oNwgmf3s0A297pLQ=";
   };
 
   patches =
